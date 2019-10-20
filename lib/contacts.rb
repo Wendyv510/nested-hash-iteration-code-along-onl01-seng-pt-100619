193211ab3397a6cc21d42adbd51f83eeebc45936
Wendy Harris <wendyv510@hotmail.com>
2019-10-20 21:19:54 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,11 @@ require 'pry'
   
 def remove_strawberry(contacts)
     contacts.each do |person, contact_details_hash|
+      if person == "Freddy Mercury"
+        contact_details_hash.each do |attritbute, data|
       binding.pry 
     end
+  end 
 end
 
 remove_strawberry(contacts)
