34dd55879cfc9f15c1120d8d0cd1b92e02aed6f6
Wendy Harris <wendyv510@hotmail.com>
2019-10-20 21:23:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,8 +18,12 @@ def remove_strawberry(contacts)
     contacts.each do |person, contact_details_hash|
       if person == "Freddy Mercury"
         contact_details_hash.each do |attritbute, data|
-      binding.pry 
+          if attribute == :favorite_ice_cream_flavors
+            data.delete_if{|ice_cream| ice_cream == "strawberry"}
+      
     end
+  end 
+  end
   end 
 end
 
